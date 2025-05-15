2c52e5d44f3c7f7d9bf5459fe7208ba0ab035627
Diego Orozco <diego2001bom@gmail.com>
2025-05-15 13:47:49 -0600

    Feature: added tooltips and on hover effects on several elements

--- script.js
+++ script.js
@@ -6,7 +6,4 @@ menu.onClick = () => {
     nav.classList.toggle('active');
 }
 
-
-//agregar las tecnologias que domino
 //AGREGAR OPCION AL ESPAÑOL
-//agregar un hover sobre mi nombre mostrando nombre completo y tambien sobre los iconos de github, linkedin, etc y leetCode cuando este decente
